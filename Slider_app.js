3b5f4a24b1e370ce06bacd5b40e182651d784e0a
hithub23 <61545337+hithub23@users.noreply.github.com>
2022-03-03 13:40:49 +0500

    Add files via upload

---
+++ Slider_app.js
@@ -0,0 +1,39 @@
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const slideCount = mainSlide.querySelectorAll('div').length
+
+const container = document.querySelector('.container')
+
+sidebar.style.top = `-${(slideCount - 1) * 100}vh`
+
+let activeSlideIndex = 0
+
+upButton.addEventListener('click', () => {
+    changeSlide('up')
+})
+
+downButton.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex === slideCount) {
+            activeSlideIndex = 0
+        }
+    }else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slideCount - 1
+        }
+    }
+    const height = container.clientHeight
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
